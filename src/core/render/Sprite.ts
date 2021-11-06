7e12ce754e7e21db30c97ef06a61898c02d883eb
MrTowers <dawmc52@gmail.com>
2021-11-06 13:03:09 +0100

    sprite

--- src/core/render/Sprite.ts
+++ src/core/render/Sprite.ts
@@ -3,15 +3,20 @@ import { GameObject } from "../objects/GameObject.js";
 
 export class Sprite extends Component {
     image: HTMLImageElement;
+    sizeX: number;
+    sizeY: number;
 
     constructor (image = new Image()) {
         super();
         this.image = image;
+        this.sizeX = 20;
+        this.sizeY = 20;
     }
 
-    render () {
+    render (ctx: CanvasRenderingContext2D) {
         if (this.image.width > 0 && this.image.height > 0) {
-            
+            let pos = this.gameObject.getPosition();
+            ctx.drawImage(this.image, pos.x, pos.y);
         }
     }
 }
