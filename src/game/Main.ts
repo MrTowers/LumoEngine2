2d05b7c20b39a5406635500355f5a9e936470dde
MrTowers <dawmc52@gmail.com>
2021-11-06 19:23:15 +0100

    robimy next day

--- src/game/Main.ts
+++ src/game/Main.ts
@@ -1,15 +1,3 @@
-import { spawnGameObject } from "../core/functions.js";
-import { Load } from "../core/Load.js";
-import { Color } from "../core/math/Color.js";
-import { GameObject } from "../core/objects/GameObject.js";
-import { Box } from "../core/render/Box.js";
-import { Engine } from "../core/render/Engine.js";
-import { Sprite } from "../core/render/Sprite.js";
-import { Tilemap } from "../core/render/Tilemap.js";
-import { RIM_WORLD } from "../RimWorld.js";
-import { PlayerController } from "./Control/PlayerController.js";
-import { Entity } from "./Entity.js";
-
 export function main () {
     
 }
