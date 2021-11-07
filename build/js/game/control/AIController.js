7013d16c3efb87d6ac81a8c935f8e0e508aca869
MrTowers <dawmc52@gmail.com>
2021-11-07 13:16:16 +0100

    xx

--- build/js/game/control/AIController.js
+++
@@ -1,9 +0,0 @@
-import { Vector2 } from "../../core/math/Vector2.js";
-import { Component } from "../../core/objects/Component.js";
-export class AIController extends Component {
-    constructor() {
-        super();
-        this.targetLocation = new Vector2();
-        this.state = "idle";
-    }
-}
