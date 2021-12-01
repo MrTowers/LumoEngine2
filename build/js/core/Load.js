a81be0cf7b0505cf5d134cb095d118d5eed98173
MrTowers <dawmc52@gmail.com>
2021-12-01 10:59:51 +0100

    x

--- build/js/core/Load.js
+++ build/js/core/Load.js
@@ -9,4 +9,16 @@ export class Load {
         x.src = `../src/game/${src}`;
         LUMO_ENGINE2.textures[name] = x;
     }
+    static file(src = "", callback = (data) => { }) {
+        let x = new XMLHttpRequest();
+        x.open("get", src);
+        x.onload = () => {
+            callback(x.responseText);
+        };
+        x.send();
+    }
+    static audio(src = "") {
+        let audio = new Audio(src);
+        audio.load();
+    }
 }
--- src/core/Load.ts
+++ src/core/Load.ts
@@ -7,9 +7,23 @@
 import { LUMO_ENGINE2 } from "../LumoEngine2.js";
 
 export class Load {
-    static texture (src = "", name = "") {
+    static texture(src = "", name = "") {
         let x = new Image();
         x.src = `../src/game/${src}`;
         LUMO_ENGINE2.textures[name] = x;
+    }
+
+    static file(src = "", callback = (data: string) => {}) {
+        let x = new XMLHttpRequest();
+        x.open("get", src);
+        x.onload = () => {
+            callback(x.responseText);
+        }
+        x.send();
+    }
+
+    static audio (src = "") {
+        let audio = new Audio(src);
+        audio.load();
     }
 }
