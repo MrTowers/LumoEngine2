a5bedddef92af7ccf4162accb0be6490887beee9
MrTowers <dawmc52@gmail.com>
2021-12-01 10:49:18 +0100

    x

--- src/game/Main.ts
+++ src/game/Main.ts
@@ -1,9 +1,3 @@
-import { spawnGameObject } from "../core/functions/spawnGameObject.js";
-import { Load } from "../core/Load.js";
-import { Player } from "./prefabs/Player.js";
-
 export function main() {
-    Load.texture("./image.png", "texture");
-
-    spawnGameObject(new Player());
+    
 }
